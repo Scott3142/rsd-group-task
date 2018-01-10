f901e412c587e3e4f49d3b579f2a0e82fdc816af
Scott Morgan <MorganSN@cardiff.ac.uk>
2018-01-10 15:36:44 +0000

    Added a file to carry out data analysis on pandas dataframe

---
+++ src/run_data_analysis.py
@@ -0,0 +1,17 @@
+'''
+Function to run simple data analysis on data set housing.csv
+'''
+
+import total_column
+import data_read_in_function as dr
+
+def run_analysis():
+
+    file_path = '../data/housing.csv'
+    data_frame = dr.data_read_in(file_path)
+
+    total_rooms = total_column.sum(data_frame,'total_rooms')
+
+    return total_rooms
+
+run_analysis()
--- src/total_column.py
+++ src/total_column.py
@@ -2,6 +2,6 @@
 Function to sum column of pandas data set given as input.
 '''
 
-def total_column(data_frame,column_title):
+def sum(data_frame,column_title):
 
     return  data_frame[column_title].sum()
